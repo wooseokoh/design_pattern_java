1d0481f2b6f2b105e4bbcc1a8547ebb389938a6c
wooseokoh <allsafe1206@gmail.com>
2023-06-23 22:49:16 +0900

    12. memento pattern

---
+++ src/Memento/Walker.java
@@ -0,0 +1,56 @@
+package Memento;
+
+import java.util.ArrayList;
+
+public class Walker {
+    private int currentX, currentY;
+    private int targetX, targetY;
+    private ArrayList<String> actionList = new ArrayList<String>();
+
+    public Walker(int currentX, int currentY, int targetX, int targetY) {
+        this.currentX = currentX;
+        this.currentY = currentY;
+        this.targetX = targetX;
+        this.targetY = targetY;
+    }
+
+    public double walk(String action){
+        actionList.add(action);
+
+        if(action.equals("UP")){
+            currentY += 1;
+        }else if(action.equals("RIGHT")){
+            currentX += 1;
+        }else if(action.equals("DOWN")){
+            currentY -= 1;
+        }else if(action.equals("LEFT")){
+            currentY -= 1;
+        }
+        return Math.sqrt(Math.pow(currentX-targetX,2)+Math.pow(currentY-targetY,2));
+    }
+
+    public class Memento{
+        private int x,y;
+        private ArrayList<String> actionList;
+        private Memento(){}
+    }
+
+    public Memento createMemento(){
+        Memento memento = new Memento();
+        memento.x = this.currentX;
+        memento.y = this.currentY;
+        memento.actionList = (ArrayList<String>)this.actionList.clone();
+        return memento;
+    }
+
+    public void restoreMemento(Memento memento){
+        this.currentX = memento.x;
+        this.currentY = memento.y;
+        this.actionList = (ArrayList<String>)this.actionList.clone();
+    }
+
+    @Override
+    public String toString() {
+        return actionList.toString();
+    }
+}
